23f1cd814db0b1cba0baa2dceb7dc9808f6497d3
sunimalherath <sunimaldv@gmail.com>
2021-03-11 01:10:34 +1100

    adding base to templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,19 +1,71 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
+var functions = template.FuncMap{}
+
 // RenderTemplate renders templates using text/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// 1. Parse file
-	parsedTmpl, _ := template.ParseFiles("./templates/" + tmpl)
-
-	// 2. Execute parsed file
-	err := parsedTmpl.Execute(w, nil)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template", err)
+		log.Fatalln(err)
 	}
+
+	t, ok := tc[tmpl]
+
+	if !ok {
+		log.Fatalln(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
+
+	if err != nil {
+		fmt.Println("error writing template to browser", err)
+	}
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// create a template set
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
