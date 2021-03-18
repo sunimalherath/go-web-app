2f97d725413230c4f2add6a3d5ea67d94beee025
sunimalherath <sunimaldv@gmail.com>
2021-03-19 00:23:16 +1100

    Optimizing template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,10 +19,14 @@ func main() {
 	}
 
 	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	http.ListenAndServe(portNumber, nil)
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,17 +1,37 @@
 package handlers
 
 import (
+	"github.com/sunimalherath/go-web-app/pkg/config"
 	"net/http"
 
 	"github.com/sunimalherath/go-web-app/pkg/render"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository fo the handelers.
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home - this is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About - this is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
