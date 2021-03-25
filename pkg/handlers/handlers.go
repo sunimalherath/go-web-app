8914238b2d70908c80260dd9603377ee0cdab69e
sunimalherath <sunimaldv@gmail.com>
2021-03-25 22:37:57 +1100

    adding Template struct to pass data

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,6 +7,17 @@ import (
 	"github.com/sunimalherath/go-web-app/pkg/render"
 )
 
+// TemplateData holds data sent from handlers to templates
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap map[string]int
+	FloatMap map[string]float32
+	Data map[string]interface{}
+	CSRFToken string
+	Flash string
+	Warning string
+	Error string
+}
 // Repo the repository used by the handlers
 var Repo *Repository
 
