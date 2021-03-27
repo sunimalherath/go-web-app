796ab3234306f2ff5593f8616f5cd352f4b392ee
sunimalherath <sunimaldv@gmail.com>
2021-03-27 23:08:32 +1100

    using chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/sunimalherath/go-web-app/pkg/config"
 	"github.com/sunimalherath/go-web-app/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	//mux := pat.New()
+	//
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about",http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about",http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Get("/",handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sunimalherath/go-web-app
 
 go 1.16
 
-require github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
+require github.com/go-chi/chi/v5 v5.0.2
