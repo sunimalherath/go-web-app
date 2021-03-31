55c05d647bf7a528255432b025e08aeca31a0c80
sunimalherath <sunimaldv@gmail.com>
2021-03-31 23:30:48 +1100

    using session to store ip and retreive it

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,6 +30,9 @@ func NewHandlers(r *Repository) {
 
 // Home - this is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -37,6 +40,9 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, friend"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
